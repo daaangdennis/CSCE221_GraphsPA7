bc4a94052f99a215a3029dd53d3a875035222d45
Dennis Dang <113410209+daaangdennis@users.noreply.github.com>
2023-04-17 22:20:40 -0500

    Fixed computeIndegrees()

--- src/top-sort-helpers.h
+++ src/top-sort-helpers.h
@@ -8,16 +8,21 @@
 template <typename T>
 void computeIndegrees(const WeightedGraph<T>& graph, std::unordered_map<value_type<T>, int>& indegrees) {
     // TODO store the indegree for each vertex in the graph in the indegrees map
-    for(value_type<T> vertex: graph)
+
+    // Set each indegree initially to 0
+    for(auto it = graph.begin(); it != graph.end(); it++)
     {
+        value_type<T> vertex = it->first;
         indegrees[vertex] = 0;
     }
 
-    for(adjacency_list<T> adj_list: graph)
+    // Compute indegrees
+    for(auto it = graph.begin(); it != graph.end(); it++)
     {
-        for(value_type<T> vertex: adj_list)
+        value_type<T> vertex = it->first;
+        for(auto [dst_vertex, weight]: graph.at(vertex))
         {
-            indegrees[vertex] += 1;
+            indegrees[dst_vertex] += 1;
         }
     }
 }
